02563f6de2a309a6e381f400c89de2ce06e7911d
VadimsMalickis <vadims.malickis@rvt.lv>
2023-12-02 12:55:47 +0200

    minor change

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -2,13 +2,9 @@ package rvt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 
-@Controller
 @SpringBootApplication
 public class App {
-
-  
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
