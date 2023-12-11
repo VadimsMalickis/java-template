63746bda07cd07babb82bb51296e8e2aa53741bd
VadimsMalickis <vadims.malickis@rvt.lv>
2023-12-11 11:21:00 +0200

    template update

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,5 +1,9 @@
 package rvt;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,12 @@ public class DefaultController {
     
    @GetMapping(value = "/")
     String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+        Student student = new Student("John", "Smith", "a@a.lv", "DP2-4");
+        List<Student> students = new ArrayList<>();
+        students.add(student);
+        model.addAttribute("students", students);
+        model.addAttribute("date", new Date().toString());
+        
         return "index";
     }
 }
