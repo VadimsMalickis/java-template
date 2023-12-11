ae28d56ac6fd132a0de3f9d9b83cb3b4d0ae38f4
VadimsMalickis <vadims.malickis@rvt.lv>
2023-12-11 11:25:38 +0200

    template update

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -5,21 +5,22 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class DefaultController {
     
-   @GetMapping(value = "/")
-    String index(@RequestParam(name="name", required=false, defaultValue="null") String name, Model model) {
+    @GetMapping(value = "/")
+    ModelAndView index(@RequestParam(name="name", required=false, defaultValue="null") String name) {
         Student student = new Student("John", "Smith", "a@a.lv", "DP2-4");
         List<Student> students = new ArrayList<>();
         students.add(student);
-        model.addAttribute("students", students);
-        model.addAttribute("date", new Date().toString());
-        
-        return "index";
+
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("students", students);
+        modelAndView.addObject("date", new Date().toString());
+        return modelAndView;
     }
 }
