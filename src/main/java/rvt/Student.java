685b339f3d18fdabe0b065d6dd10f2791f564327
Vadims Malickis <117085106+VadimsMalickis@users.noreply.github.com>
2024-02-02 07:17:06 +0000

    small refactor

--- src/main/java/rvt/Student.java
+++ src/main/java/rvt/Student.java
@@ -5,16 +5,24 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 public class Student {
     private String name;
+
     private String surname;
+
     private int age;
+
     private String password;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+
     private String gender;
+
     private boolean married;
+
     private String note;
+
     private String email;
+
     private String group;
     
     public String getName() {
@@ -98,7 +106,19 @@ public class Student {
     }
     
     public String toString() {
-        return String.format("{%s, %s, %s, %s}", this.name, this.surname, this.email, this.group);
+        return String.format(
+            "{%s, %s, %s, %s, %s, %s, %s, %s, %s, %s}",
+            this.name,
+            this.surname,
+            this.age,
+            this.password,
+            this.birthday,
+            this.gender,
+            this.married,
+            this.note,
+            this.email,
+            this.group
+        );
     }
 
 }
