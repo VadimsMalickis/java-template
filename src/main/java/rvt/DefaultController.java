f97118326663c71c9829087065ac876d00b04eb4
VadimsMalickis <vadims.malickis@rvt.lv>
2024-02-02 09:53:11 +0200

    work on registration

--- src/main/java/rvt/DefaultController.java
+++ src/main/java/rvt/DefaultController.java
@@ -1,5 +1,6 @@
 package rvt;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -14,54 +15,44 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class DefaultController {
 
     @GetMapping(value = "/")
     ModelAndView index() {
+        // TODO get students from CSV
         ArrayList<Student> students = new ArrayList<>();
-
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("students", students);
         modelAndView.addObject("date", new Date().toString());
         return modelAndView;
     }
 
-    @GetMapping(value = "/about")
-    ModelAndView about(@RequestParam HashMap<String, String> queryStringParams) {
-            ModelAndView modelAndView = new ModelAndView("about");
-
-            String id = queryStringParams.get("id");
-            String title = queryStringParams.get("title");
-            String description = queryStringParams.get("description");
-
-            // turpinat veidot html formu
-
-            return modelAndView;
-    }
-
-
-    // Metode priekš koda testēšanas
     @GetMapping(value = "/test")
     public ModelAndView testAction() {
         ModelAndView modelAndView = new ModelAndView("test");
-
-        
         modelAndView.addObject("output1", 1);
-     
+        return modelAndView;
+    }
+
+    @GetMapping(value = "/register")
+    public ModelAndView register(@ModelAttribute("student") Student student) {
+
+        ModelAndView modelAndView = new ModelAndView("registration/registration-page");
+        List<String> groups = new ArrayList<>(List.of("DP2-1", "DP2-2", "DP2-3", "DP2-4"));
+        modelAndView.addObject("groups", groups);
+        modelAndView.addObject("student", student);
 
         return modelAndView;
     }
 
-    @RequestMapping(value = "/register", method = {RequestMethod.GET, RequestMethod.POST})
+    @PostMapping(value = "/register")
     public ModelAndView register(HttpServletRequest request, @ModelAttribute("student") Student student) {
-        Student newStudent = new Student();
-        ModelAndView modelAndView = new ModelAndView("registration/registration-page");
-        List<String> groups = new ArrayList<>(List.of("DP2-1", "DP2-2", "DP2-3", "DP2-4"));
-        modelAndView.addObject("student", newStudent);
-        modelAndView.addObject("groups", groups);
-        System.out.println(student);
+        ModelAndView modelAndView = new ModelAndView("registration/registration-success");
+
         return modelAndView;
     }
 
--- src/main/java/rvt/Student.java
+++ src/main/java/rvt/Student.java
@@ -12,7 +12,6 @@ public class Student {
 
     private String password;
 
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
 
     private String gender;
@@ -24,101 +23,100 @@ public class Student {
     private String email;
 
     private String group;
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public String getSurname() {
         return surname;
     }
-    
+
     public void setSurname(String surname) {
         this.surname = surname;
     }
-    
+
     public int getAge() {
         return age;
     }
-    
+
     public void setAge(int age) {
         this.age = age;
     }
-    
+
     public String getPassword() {
         return password;
     }
-    
+
     public void setPassword(String password) {
         this.password = password;
     }
-    
+
     public LocalDate getBirthday() {
         return birthday;
     }
-    
+
     public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
     }
-    
+
     public String getGender() {
         return gender;
     }
-    
+
     public void setGender(String gender) {
         this.gender = gender;
     }
-    
+
     public boolean isMarried() {
         return married;
     }
-    
+
     public void setMarried(boolean married) {
         this.married = married;
     }
-    
+
     public String getNote() {
         return note;
     }
-    
+
     public void setNote(String note) {
         this.note = note;
     }
-    
+
     public String getEmail() {
         return email;
     }
-    
+
     public void setEmail(String email) {
         this.email = email;
     }
-    
+
     public String getGroup() {
         return group;
     }
-    
+
     public void setGroup(String group) {
         this.group = group;
     }
-    
+
     public String toString() {
         return String.format(
-            "{%s, %s, %s, %s, %s, %s, %s, %s, %s, %s}",
-            this.name,
-            this.surname,
-            this.age,
-            this.password,
-            this.birthday,
-            this.gender,
-            this.married,
-            this.note,
-            this.email,
-            this.group
-        );
+                "{%s, %s, %s, %s, %s, %s, %s, %s, %s, %s}",
+                this.name,
+                this.surname,
+                this.age,
+                this.password,
+                this.birthday,
+                this.gender,
+                this.married,
+                this.note,
+                this.email,
+                this.group);
     }
 
 }
